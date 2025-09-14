70a7e1cbd6c7991069f28b25e3e793c26fae9cc2
Qudn1m <117811649+Qudn1m@users.noreply.github.com>
2025-09-14 16:24:48 +0700

    Add files via upload

---
+++ tugas3/tugas3.py
@@ -0,0 +1,31 @@
+int_  = 90
+string = "kamera"
+float = 0.17
+bool = False
+list_ = [1,2,3,4,5]
+print(int)
+print(str)
+print(float)
+print(list_)
+
+string_baru = string.upper()
+print("\n" + string_baru)
+
+int2 = 35
+mtk = int_*int2
+print(mtk)
+
+newlist = [66, 33]
+list_.append(newlist)
+print(list_)
+list_.pop(2)
+print(list_)
+
+print("perkenalan singkat ")
+str_name = input("siapa nama anda")
+age_str = input("masukkan umur anda: ")
+age_int = int(age_str)
+print("umur anda", age_int, "tahun.")
+print("Halo, ", str_name,"dan umur anda ", age_int, " tahun.")
+
+
